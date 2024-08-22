340b55f5a0347055ced58166da454f57708f5672
fr13nds <fr13nds.2022@gmail.com>
2024-08-22 05:31:39 +0300

    added multiline

---
+++ after/plugin/multicursor.lua
@@ -0,0 +1,3 @@
+vim.keymap.set('n', '<C-d>', '<Plug>(VM-Find-Under)', { noremap = true, silent = true })
+vim.keymap.set('x', '<C-d>', '<Plug>(VM-Find-Subword-Under)', { noremap = true, silent = true })
+
--- lua/theprimeagen/packer.lua
+++ lua/theprimeagen/packer.lua
@@ -4,73 +4,72 @@
 vim.cmd.packadd("packer.nvim")
 
 return require("packer").startup(function(use)
-  -- Packer can manage itself
-  use "wbthomason/packer.nvim"
+    -- Packer can manage itself
+    use "wbthomason/packer.nvim"
+    use { "mg979/vim-visual-multi", branch = "master" }
 
-  use {
-	  "nvim-telescope/telescope.nvim", branch = "0.1.x",
-	  -- or                            , tag = "0.1.x",
-	  requires = { {"nvim-lua/plenary.nvim"} }
-  }
+    use {
+        "nvim-telescope/telescope.nvim", branch = "0.1.x",
+        -- or                            , tag = "0.1.x",
+        requires = { { "nvim-lua/plenary.nvim" } }
+    }
 
-  use("folke/tokyonight.nvim")
+    use("folke/tokyonight.nvim")
 
-  use({
-	  "rose-pine/neovim",
-	  as = "rose-pine",
-	  config = function()
-		  vim.cmd("colorscheme rose-pine")
-	  end
-  })
-
-  use({
-      "folke/trouble.nvim",
-      config = function()
-          require("trouble").setup {
-              icons = false,
-              -- your configuration comes here
-              -- or leave it empty to use the default settings
-              -- refer to the configuration section below
-          }
-      end
-  })
-
-  use { 'nvim-treesitter/nvim-treesitter',
+    use({
+        "rose-pine/neovim",
+        as = "rose-pine",
         config = function()
-            require("nvim-treesitter.install").compilers = {"gcc" }
+            vim.cmd("colorscheme rose-pine")
+        end
+    })
+
+    use({
+        "folke/trouble.nvim",
+        config = function()
+            require("trouble").setup {
+                icons = false,
+                -- your configuration comes here
+                -- or leave it empty to use the default settings
+                -- refer to the configuration section below
+            }
+        end
+    })
+
+    use { 'nvim-treesitter/nvim-treesitter',
+        config = function()
+            require("nvim-treesitter.install").compilers = { "gcc" }
         end,
     }
-  use("theprimeagen/harpoon")
-  use("mbbill/undotree")
-  use("tpope/vim-fugitive")
-  use("nvim-treesitter/nvim-treesitter-context");
+    use("theprimeagen/harpoon")
+    use("mbbill/undotree")
+    use("tpope/vim-fugitive")
+    use("nvim-treesitter/nvim-treesitter-context");
 
-  use("epwalsh/obsidian.nvim")
+    use("epwalsh/obsidian.nvim")
 
-  use {
-	  "VonHeikemen/lsp-zero.nvim",
-	  branch = "v4.x",
-	  requires = {
-		  -- LSP Support
-		  {"neovim/nvim-lspconfig"},
-		  {"williamboman/mason.nvim"},
-		  {"williamboman/mason-lspconfig.nvim"},
+    use {
+        "VonHeikemen/lsp-zero.nvim",
+        branch = "v4.x",
+        requires = {
+            -- LSP Support
+            { "neovim/nvim-lspconfig" },
+            { "williamboman/mason.nvim" },
+            { "williamboman/mason-lspconfig.nvim" },
 
-		  -- Autocompletion
-		  {"hrsh7th/nvim-cmp"},
-		  {"hrsh7th/cmp-buffer"},
-		  {"hrsh7th/cmp-path"},
-		  {"saadparwaiz1/cmp_luasnip"},
-		  {"hrsh7th/cmp-nvim-lsp"},
-		  {"hrsh7th/cmp-nvim-lua"},
+            -- Autocompletion
+            { "hrsh7th/nvim-cmp" },
+            { "hrsh7th/cmp-buffer" },
+            { "hrsh7th/cmp-path" },
+            { "saadparwaiz1/cmp_luasnip" },
+            { "hrsh7th/cmp-nvim-lsp" },
+            { "hrsh7th/cmp-nvim-lua" },
 
-		  -- Snippets
-		  {"L3MON4D3/LuaSnip"},
-		  {"rafamadriz/friendly-snippets"},
-	  }
-  }
+            -- Snippets
+            { "L3MON4D3/LuaSnip" },
+            { "rafamadriz/friendly-snippets" },
+        }
+    }
 
-  use("eandrju/cellular-automaton.nvim")
-
+    use("eandrju/cellular-automaton.nvim")
 end)
-
